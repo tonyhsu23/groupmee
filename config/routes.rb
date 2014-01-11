37919ef80fda1b3cc6f54d57203ea07a9a532ff8
Tony Hsu <tony@TonydeMacBook-Air.local>
2014-01-11 22:37:43 +0800

    groups CRUD (finish chapter1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Groupmee::Application.routes.draw do
+  
+  resources :groups
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root :to => 'high_voltage/pages#show', :id => 'welcome'
+  root :to => "groups#index"
 end
