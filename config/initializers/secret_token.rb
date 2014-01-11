c0e5e85924319ba438bc2fd13af505cd08bc43d7
Tony Hsu <tony@TonydeMacBook-Air.local>
2014-01-11 17:52:14 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupmee::Application.config.secret_key_base = 'ecefd5b69da92a5d33dd337304774dd82869e0b328c9a8b8a90fb8ec755eb5e4f6cc83fe13a356f0cd377dd5f94f299d752ec37741b812e1345cedf56c87a3cd'
