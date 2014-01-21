de9b5b53fafedd16b74f518a3087c376397f742d
Tony Hsu <tony@TonydeMacBook-Air.local>
2014-01-21 16:55:09 +0800

    add back-end system

---
+++ app/controllers/account/posts_controller.rb
@@ -0,0 +1,8 @@
+class Account::PostsController < ApplicationController
+	before_action :login_required
+
+	def index
+		@posts = current_user.posts.order("updated_at DESC")
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Groupmee::Application.routes.draw do
   	resources :posts
   end
 
+  namespace :account do
+  	resources :groups
+  	resources :posts
+  end
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root :to => "groups#index"
 end
